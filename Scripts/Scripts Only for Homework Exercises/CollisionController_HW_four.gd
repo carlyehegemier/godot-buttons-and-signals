387fab37925233740a1d63c7781d65c647a6c9cd
carlyehegemier <cehegemier@gmail.com>
2024-02-28 12:52:32 -0500

    Scene 4 finished

--- Scripts/Scripts Only for Homework Exercises/CollisionController_HW_four.gd
+++ Scripts/Scripts Only for Homework Exercises/CollisionController_HW_four.gd
@@ -13,3 +13,11 @@ func _enable_collision():
 
 
 
+
+
+func _on_area_2d_body_entered(body):
+	pass # Replace with function body.
+
+
+func _on_area_2d_body_exited(body):
+	pass # Replace with function body.
